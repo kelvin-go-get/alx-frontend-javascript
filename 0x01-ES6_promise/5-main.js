5e7dfd512d1abe5dab0cefe4ccd2591d76d76f87
Kelvin <knjuiri@gmail.com>
2024-06-25 11:52:11 +0300

    updates

---
+++ 0x01-ES6_promise/5-main.js
@@ -0,0 +1,4 @@
+// 5-main.js
+import uploadPhoto from "./5-photo-reject";
+
+console.log(uploadPhoto("guillaume.jpg"));
---
+++ 0x01-ES6_promise/9-main.js
@@ -0,0 +1,6 @@
+// 9-main.js
+import guardrail from "./9-try";
+import divideFunction from "./8-try";
+
+console.log(guardrail(() => divideFunction(10, 2)));
+console.log(guardrail(() => divideFunction(10, 0)));
