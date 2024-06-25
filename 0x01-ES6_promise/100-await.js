89ab779cb975959ff1d6a8776e15cd7e18360257
Kelvin <knjuiri@gmail.com>
2024-06-25 12:25:18 +0300

    updates

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,29 @@
+// asyncUploadUser function in 100-await.js
+
+import { uploadPhoto, createUser } from "./utils.js";
+
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    // Await both promises simultaneously
+    const [photoResult, userResult] = await Promise.all([
+      photoPromise,
+      userPromise,
+    ]);
+
+    return {
+      photo: photoResult,
+      user: userResult,
+    };
+  } catch (error) {
+    console.error("Error occurred:", error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
