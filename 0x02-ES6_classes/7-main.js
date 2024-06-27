72752286f1830af29e3e933f60325e90e059ae4e
Kelvin <knjuiri@gmail.com>
2024-06-27 15:02:50 +0300

    updates

---
+++ 0x02-ES6_classes/7-main.js
@@ -0,0 +1,7 @@
+// 7-main.js
+
+import Airport from "./7-airport.js";
+
+const airportSF = new Airport("San Francisco Airport", "SFO");
+console.log(airportSF); // Airport [SFO] { _name: 'San Francisco Airport', _code: 'SFO' }
+console.log(airportSF.toString()); // [SFO]
---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+// 8-hbtn_class.js
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Method to return size when cast to Number
+  valueOf() {
+    return this._size;
+  }
+
+  // Method to return location when cast to String
+  toString() {
+    return this._location;
+  }
+}
